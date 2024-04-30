49c32446cd038fe55aa03f33c66ed5d182d39263
Андрей Барышников <andrey1andrey@narod.ru>
2024-04-30 11:05:51 +0300

    add 745

---
+++ Glava_26/Str736/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Str736
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main thread: starting a dedicated thread to do an asynchronous operation");
+            Thread dedicatedThread = new Thread(ComputeBoundOp);
+            dedicatedThread.Start(5);
+
+            Console.WriteLine("Main thread: Doing other work here ...");
+            Thread.Sleep(10_000);  // имитация другой работы
+
+            dedicatedThread.Join(); // ожидание завершения потока
+            Console.WriteLine("Hit <Enter> to end this program...");
+            Console.ReadLine();
+        }
+
+        // Сигнатура метода должна совпадать 
+        // с сигнатурой  делегата ParametrizedThreadStart
+        private static void ComputeBoundOp(Object state)
+        {
+            // Метод выполняемый выделенным потоком
+            Console.WriteLine("In ComputeBoundOp: state={0}", state);
+            Thread.Sleep(1_000);
+
+            // После возвращения методом управления выделеный поток завершается
+        }
+    }
+}
---
+++ Glava_26/Str745/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+public static class Program
+{
+    public static void Main()
+    {
+        // Создание нового потока (по умолчанию активного)
+        Thread t = new Thread(Worker);
+
+        // превращяем роток в фоновый
+        t.IsBackground = true;
+
+        t.Start(); // старт потока
+        // в случае активного потока приложение будет работать ещё 10 секунд
+        // в случае фонового потока приложение немедленно прекратит работу
+        Console.WriteLine("Returning from Main");
+    }
+
+    private static void Worker()
+    {
+        Thread.Sleep(10_000); // имитация 10 секунд работы
+
+        // следующая строка выводится только для кода 
+        // исполняемого активным потоком
+        Console.WriteLine("Returning from Worker");
+    }
+}
