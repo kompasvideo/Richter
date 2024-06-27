22601d93bd4bb7cbd02dd6666e9489feae973ed0
andrejbarysnikov <andrey1andrey@narod.ru>
2024-06-27 21:59:10 +0300

    Str771

--- Glava_27/Str767/Program.cs
+++ Glava_27/Str767/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Str767
+﻿using System.Text;
+
+namespace Str767
 {
     internal class Program
     {
@@ -12,15 +14,56 @@
                     TaskScheduler.Default);
 
                 // Задание создаёт и запускает 3 дочерних задания
-
+                var childTasks = new[]
+                {
+                    tf.StartNew(() => Sum(cts.Token, 10_000)),
+                    tf.StartNew(() => Sum(cts.Token, 20_000)),
+                    tf.StartNew(() => Sum(cts.Token, Int32.MaxValue)) // исключение OverflowException
+                }; 
+                // Если дочернее задание становиться источником исключения,
+                // отменяем все дочерние задания
+                for (Int32 task = 0; task < childTasks.Length; task++)
+                    childTasks[task].ContinueWith(t => cts.Cancel(), TaskContinuationOptions.OnlyOnFaulted);
+                
+                // После завершения дочерних заданий получаем максимальное
+                // возвращяемое значение и передаем его другому заданию
+                // для вывода
+                tf.ContinueWhenAll(childTasks, 
+                        completedTasks => completedTasks.Where(
+                            t => !t.IsFaulted && !t.IsCanceled).Max(t => t.Result),
+                        CancellationToken.None)
+                    .ContinueWith(t => Console.WriteLine("The maximum is: " + t.Result),
+                        TaskContinuationOptions.ExecuteSynchronously);
             });
-            Console.WriteLine("Hello, World!");
+                            
+            // После завершения дочерних заданий выводим,
+            // в том числе и необработанные исключения
+            parent.ContinueWith(p =>
+            {
+                // Текст помещён в StringBuilder и однократно вызван
+                // метод Console.WriteLine просто потому, что это задание
+                // может выполняться паралельно с предыдущим,
+                // и я не хочу в путаницы в выводимом результате
+                StringBuilder sb = new StringBuilder(
+                    "The following exception(s) occurred: " + Environment.NewLine);
+
+                foreach (var e in p.Exception.Flatten().InnerExceptions)
+                    sb.AppendLine(" " + e.GetType().ToString());
+                Console.WriteLine(sb.ToString());
+            }, TaskContinuationOptions.OnlyOnFaulted);
+            
+            // Запуск родительского задания, которое может запускать дочерние
+            parent.Start();
         }
-        private static Int32 Sum(Int32 n)
+        
+        private static Int32 Sum(CancellationToken ct, Int32 n)
         {
             Int32 sum = 0;
             for (; n > 0; n--)
+            {
+                ct.ThrowIfCancellationRequested();
                 checked { sum += n; }
+            }
             return sum;
         }
     }
---
+++ Glava_27/Str771/Program.cs
@@ -0,0 +1,18 @@
+﻿Int32 sum = 0;
+// один поток выполняет всю работу последовательно
+for(Int32 i = 0; i < 1_000; i++) DoWork(i);
+// Потоки из пула выполняют работу параллельно
+Parallel.For(0, 1_000, i => DoWork(i));
+
+List<int> collection = new List<int>(){ 1, 2, 3, 4 };
+// один поток выполняет всю работу по очереди
+foreach (var item in collection) DoWork(item);
+// Потоки из пула выполняют всю работу параллельно
+Parallel.ForEach(collection, item => DoWork(item));
+
+
+
+void DoWork(Int32 i)
+{
+    sum += i;
+}
