341700de6f83172a52c968898242516436735410
andrejbarysnikov <andrey1andrey@narod.ru>
2024-06-29 14:15:50 +0300

    Str776

---
+++ Glava_27/Str776/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+Console.WriteLine("Hello, World!");
+
+static void ObsoleteMethods(Assembly assembly)
+{
+    var query = from type in assembly.GetExportedTypes().AsParallel()
+        from method in type.GetMethods(BindingFlags.Public | 
+               BindingFlags.Instance | BindingFlags.Static)
+        let obsoleteAttrType = typeof(ObsoleteAttribute)
+            where Attribute.IsDefined(method, obsoleteAttrType)
+            orderby type.FullName
+                let obsoleteAttrObj = (ObsoleteAttribute)
+                    Attribute.GetCustomAttribute(method, obsoleteAttrType)
+                    select String.Format("Type={0},\nMethod={1}\nMessage={2}\n",
+                        type.FullName, method.ToString(), obsoleteAttrObj.Message);
+                    
+                    // вывод результатов
+                    foreach(var result in query) Console.WriteLine(result);
+}
