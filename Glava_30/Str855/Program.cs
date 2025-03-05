141c64450df7ae53134977f02bab9e26cf7e377d
Baryshnikov Andrey <baryshnikov@BARYLNIKOVPC>
2025-03-05 09:04:40 +0300

    1

---
+++ Glava_30/Str855/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Str855
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- TempClasses/Program.cs
+++ TempClasses/Program.cs
@@ -6,11 +6,3 @@ internal class Program
         Console.WriteLine("Hello, World!");
     }
 }
-
-
-public sealed class Semaphore : WaitHandle
-{
-    public Semaphore(Int32 initialCount, Int32 maxCount) { }
-    public Int32 Release() { return 0; }
-
-}
